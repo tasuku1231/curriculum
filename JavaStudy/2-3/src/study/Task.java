b0be8a33dc5e897e89198a496062a3c83191ec46
tasuku1231 <76238411+tasuku1231@users.noreply.github.com>
2021-03-05 21:25:53 +0900

    課題提出
    オーバーロード・継承・オーバーライド

--- JavaStudy/2-3/src/study/Task.java
+++ JavaStudy/2-3/src/study/Task.java
@@ -10,10 +10,9 @@ package study;
 
         // ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
         // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
-    	Calculator calculator = new Calculator();
-    		int a = calculator.plus(10);
-			int b = calculator.plus(10,20);
-			int c = calculator.plus(10,20,30);
+    		int a = plus(10);
+			int b = plus(10,20);
+			int c = plus(10,20,30);
 
 			System.out.println("plusメソッドの引数が一つの場合：" + a);
 			System.out.println("plusメソッドの引数が二つの場合：" + b);
