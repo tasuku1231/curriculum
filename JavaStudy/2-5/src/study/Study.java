8610fd99ff3ac3babf35c8a8aa353a4dc68fc74e
tasuku1231 <76238411+tasuku1231@users.noreply.github.com>
2021-03-19 00:34:49 +0900

    課題提出
    日付操作クラス

--- JavaStudy/2-5/src/study/Study.java
+++ JavaStudy/2-5/src/study/Study.java
@@ -35,7 +35,7 @@ public class Study {
 
         // ② 以下の「shopMap.put(shohinList.get(1), 180);」の処理について、コメントを記述してください。
         /*
-         * [引数で指定した1をキーの持つが返ってくるのでこの場合はLinkedHashMap内の牛乳が返ってくる]
+         * [引数を１と指定おり、それに一致するキー値が返ってくるのでこの場合はshohinList内の牛乳が返ってくる]
          *
          */
         LinkedHashMap<String, Integer> shopMap = new LinkedHashMap<String, Integer>();
